626c6f683d0f90d05a7a69bf68b607998a0348ee
Danilo Silva <danilo.silva@msn.com>
2023-07-25 23:24:28 -0300

    feat: Aula sobre acoplamento

--- Padroes-Projeto-Estruturais-Gof/DesignPatterns/Cliente.cs
+++ Padroes-Projeto-Estruturais-Gof/DesignPatterns/Cliente.cs
@@ -1,5 +1,11 @@
 namespace DesignPatterns;
 
+/*
+    Acoplamento é o nível de dependência que pode existir entre os módulos/classes do sistema.
+    Quanto maior o acoplamento, maior a dependência entre os módulos/classes e maior a dificuldade de manutenção do sistema.
+    Existem dois tipos de acoplamento: Acoplamento Forte e Acoplamento Fraco.
+*/
+
 public class Cliente
 {
     public Cliente(string nome)

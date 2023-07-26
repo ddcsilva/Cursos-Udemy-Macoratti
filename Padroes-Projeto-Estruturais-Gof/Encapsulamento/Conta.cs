352078a2199e9177d9777742d40a11b8f416c4b1
Danilo Silva <danilo.silva@msn.com>
2023-07-26 15:24:06 -0300

    feat: Aula sobre Encapsulamento

---
+++ Padroes-Projeto-Estruturais-Gof/Encapsulamento/Conta.cs
@@ -0,0 +1,22 @@
+﻿namespace Encapsulamento;
+
+class Conta
+{
+    private decimal saldo;
+
+    public decimal Saldo
+    {
+        get { return saldo; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Saldo não pode ser negativo");
+            }
+            else
+            {
+                saldo = value;
+            }
+        }
+    }
+}
---
+++ Padroes-Projeto-Estruturais-Gof/Encapsulamento/Program.cs
@@ -0,0 +1,13 @@
+﻿using Encapsulamento;
+
+var conta1 = new Conta();
+conta1.Saldo = -100;
+
+try
+{
+    conta1.Saldo = -100;
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine(e.Message);
+}
